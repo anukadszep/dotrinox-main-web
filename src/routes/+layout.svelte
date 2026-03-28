6ac3d2088d35ffdd81c67ab7aefbaa2c4eb88cb4
anukadszep <m1csibor6552@proton.me>
2026-03-28 21:53:55 +0000

    WIP page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import "../app.css";
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/98.css"
+>
+<img src="wallpaper.webp" alt="background" class="fixed inset-0 w-full h-full object-cover object-center -z-10">
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="flex h-screen justify-center items-center">
+    <div class="window" style="width: 360px">
+    <div class="title-bar">
+        <div class="title-bar-text">Message from dotrinox.net</div>
+    </div>
+    <div class="flex items-center justify-center window-body text-center font-bold italic">
+        <img src="gif1.gif" alt="gif" class="h-8 mr-4">
+        <p>This page is still in progress..</p>
+        <img src="gif2.gif" alt="gif" class="h-8 ml-4">
+    </div>
+    </div>
+</div>
